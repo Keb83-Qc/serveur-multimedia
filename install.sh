c3257921c4d9feaaf8bfa2d1c414919f819416e8
Keb83-Qc <94471179+Keb83-Qc@users.noreply.github.com>
2023-11-07 16:43:41 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash 
 
+_user="$(id -u -n)"
+_uid="$(id -u)"
 # Installation de Docker
 sudo apt install -y curl software-properties-common && apt update
 sudo apt install -y docker.io
@@ -11,163 +13,166 @@ sudo chmod +x /usr/local/bin/docker-compose
 
 docker-compose -v
 
-_user="$(id -u -n)"
-_uid="$(id -u)"
-
 sudo usermod -aG docker $_user
 
 # Création fichier pour docker-compose
 cat << EOF > .env
-  TZ=America/New_York
-  PUID=$_uid
-  PGID=$_uid
+    TZ=America/New_York
+    PUID=$_uid
+    PGID=$_uid
 
-  ROOT=/home/$_user
+    ROOT=/home/$_user
+    DATA=/srv
 
-  # Tautulli
-  TAUTULLI_PORT=8181
-  # qbittorrent
-  QBT_EULA=
-  QBT_WEBUI_PORT=8080
-  QBT_PORT=6881
-  QBT_CONFIG_PATH=/home/$_user/qbittorent/config
-  QBT_DOWNLOADS_PATH=/home/$_user/qbittorent/downloads
-  # overseerr
-  OVERSERR_PORT=5055
-  # flaresolverr
-  FLARESOLVERR_PORT=8191
-  # plex https://www.plex.tv/claim/
-  PLEXCLAIM=
+    # Tautulli
+    TAUTULLI_PORT=8181
+    # qbittorrent
+    QBT_WEBUI_PORT=8080
+    QBT_PORT=6881
+    # Jackett
+    JACKETT_PORT=9117
+    # plex https://www.plex.tv/claim/
+    PLEXCLAIM=
+    # Sonarr
+    SONARR_PORT=8989
+    # Radarr
+    RADARR_PORT=7878
+    # overseerr
+    OVERSERR_PORT=5055
+    # flaresolverr
+    FLARESOLVERR_PORT=8191
 EOF
 
 cat <<- 'EOF' > docker-compose.yml
-  version: "3"
+    version: "3"
     services:
-        tautulli:
-            image: ghcr.io/tautulli/tautulli
-            container_name: tautulli
-            restart: unless-stopped
-            volumes:
-            - ${ROOT}/tautulli:/config
-            environment:
-            - PUID=${PUID}
-            - PGID=${PGID}
-            - TZ=${TZ}
-            ports:
-            - ${TAUTULLI_PORT}:${TAUTULLI_PORT}
-        qbittorrent:
-            image: lscr.io/linuxserver/qbittorrent:latest
-            container_name: qbittorrent
-            environment:
-            - PUID=${PUID}
-            - PGID=${PGID}
-            - TZ=${TZ}
-            - QBT_EULA=${QBT_EULA}
-            - QBT_WEBUI_PORT=${QBT_WEBUI_PORT}
-            volumes:
-            - ${QBT_CONFIG_PATH}:/config
-            - ${QBT_DOWNLOADS_PATH}:/downloads
-            ports:
-            - ${QBT_PORT}:${QBT_PORT}/tcp
-            - ${QBT_PORT}:${QBT_PORT}/udp
-            - ${QBT_WEBUI_PORT}:${QBT_WEBUI_PORT}
-            restart: always
-            read_only: true
-            stop_grace_period: 30m
-            tmpfs:
-            - /tmp
-            tty: true
-        jackett:
-            container_name: jackett
-            image: linuxserver/jackett:latest
-            restart: unless-stopped
-            network_mode: host
-            environment:
-            - PUID=${PUID}
-            - PGID=${PGID}
-            - TZ=${TZ}
-            volumes:
-            - /etc/localtime:/etc/localtime:ro
-            - ${ROOT}/jackett/downloads/torrent-blackhole:/downloads
-            - ${ROOT}/jackett/config/jackett:/config
-        plex:
-            container_name: plex
-            image: lscr.io/linuxserver/plex:latest
-            restart: unless-stopped
-            environment:
-            - PUID=${PUID}
-            - PGID=${PGID}
-            - TZ=${TZ}
-            - PLEX_CLAIM=${PLEXCLAIM}
-            - HOSTNAME="PlexServer"
-            network_mode: host
-            volumes:
-            - ${ROOT}/plex-server/config/plex/db:/config
-            - ${ROOT}/plex-server/config/plex/transcode:/transcode
-            - ${ROOT}/plex-server/complete:/data
-            - ${ROOT}/plex-server/tvseries:/tv
-            - ${ROOT}/plex-server/movies:/movies
-            ports:
-            - 32400:32400
-            - 1900:1900/udp
-            - 5353:5353/udp
-            - 8324:8324
-            - 32410:32410/udp
-            - 32412:32412/udp
-            - 32413:32413/udp
-            - 32414:32414/udp
-            - 32469:32469
-        sonarr:
-            container_name: sonarr
-            image: linuxserver/sonarr:latest
-            restart: unless-stopped
-            network_mode: host
-            environment:
-            - PUID=${PUID}
-            - PGID=${PGID}
-            - TZ=${TZ}
-            volumes:
-            - /etc/localtime:/etc/localtime:ro
-            - ${ROOT}/sonarr/config/sonarr:/config
-            - ${ROOT}/sonarr/complete/tv:/tv
-            - ${ROOT}/sonarr/downloads:/downloads
-        radarr:
-            container_name: radarr
-            image: linuxserver/radarr:latest
-            restart: unless-stopped
-            network_mode: host
-            environment:
-            - PUID=${PUID}
-            - PGID=${PGID}
-            - TZ=${TZ}
-            volumes:
-            - /etc/localtime:/etc/localtime:ro
-            - ${ROOT}/radarr/config/radarr:/config
-            - ${ROOT}/radarr/complete/movies:/movies
-            - ${ROOT}/radarr/downloads:/downloads
-        overseerr:
-            image: lscr.io/linuxserver/overseerr:latest
-            container_name: overseerr
-            environment:
-            - PUID=${PUID}
-            - PGID=${PGID}
-            - LOG_LEVEL=debug
-            - TZ=${TZ}
-            - PORT=${OVERSERR_PORT}
-            ports:
-            - ${OVERSERR_PORT}:${OVERSERR_PORT}
-            volumes:
-            - ${ROOT}/overseerr/appdata/config:/app/config
-            restart: unless-stopped
-        flaresolverr:
-            image: ghcr.io/flaresolverr/flaresolverr:latest
-            container_name: flaresolverr
-            environment:
-            - LOG_LEVEL=${LOG_LEVEL:-info}
-            - LOG_HTML=${LOG_HTML:-false}
-            - CAPTCHA_SOLVER=${CAPTCHA_SOLVER:-none}
-            - TZ=${TZ}
-            ports:
-            - ${FLARESOLVERR_PORT}:${FLARESOLVERR_PORT}
-            restart: unless-stopped
+
+    tautulli:
+        image: lscr.io/linuxserver/tautulli:latest
+        container_name: tautulli
+        environment:
+        - PUID=${PUID}
+        - PGID=${PGID}
+        - TZ=${TZ}
+        volumes:
+        - ${ROOT}/tautulli:/config
+        ports:
+        - ${TAUTULLI_PORT}:${TAUTULLI_PORT}
+        restart: unless-stopped
+
+    qbittorrent:
+        image: lscr.io/linuxserver/qbittorrent:latest
+        container_name: qbittorrent
+        environment:
+        - PUID=${PUID}
+        - PGID=${PGID}
+        - TZ=${TZ}
+        - QBT_WEBUI_PORT=${QBT_WEBUI_PORT}
+        volumes:
+        - ${ROOT}/qbittorent/config:/config
+        - ${DATA}/downloads:/downloads
+        ports:
+        - ${QBT_PORT}:${QBT_PORT}/tcp
+        - ${QBT_PORT}:${QBT_PORT}/udp
+        - ${QBT_WEBUI_PORT}:${QBT_WEBUI_PORT}
+        restart: unless-stopped
+
+    jackett:
+        image: lscr.io/linuxserver/jackett:latest
+        container_name: jackett
+        environment:
+        - PUID=${PUID}
+        - PGID=${PGID}
+        - TZ=${TZ}
+        - AUTO_UPDATE=true
+        volumes:
+        - /etc/localtime:/etc/localtime:ro
+        - ${ROOT}/jackett/downloads:/downloads
+        - ${ROOT}/jackett/data:/config
+        ports:
+        - ${JACKETT_PORT}:${JACKETT_PORT}
+        restart: unless-stopped
+
+    plex:
+        image: lscr.io/linuxserver/plex:latest
+        container_name: plex
+        network_mode: host
+        environment:
+        - PUID=${PUID}
+        - PGID=${PGID}
+        - TZ=${TZ}
+        - VERSION=docker
+        - PLEX_CLAIM=${PLEXCLAIM}   
+        volumes:
+        - ${ROOT}/plex/config:/config
+        - ${ROOT}/plex/transcode:/transcode
+        - ${ROOT}/plex/data:/data
+        - ${DATA}/plex/tvseries:/tv
+        - ${DATA}/plex/movies:/movies
+        ports:
+        - 32400:32400
+        - 1900:1900/udp
+        - 5353:5353/udp
+        - 8324:8324
+        - 32410:32410/udp
+        - 32412:32412/udp
+        - 32413:32413/udp
+        - 32414:32414/udp
+        - 32469:32469
+        restart: unless-stopped
+
+    sonarr:
+        image: lscr.io/linuxserver/sonarr:latest
+        container_name: sonarr
+        environment:
+        - PUID=${PUID}
+        - PGID=${PGID}
+        - TZ=${TZ}
+        volumes:
+        - /etc/localtime:/etc/localtime:ro
+        - ${ROOT}/sonarr/config:/config
+        ports:
+        - ${SONARR_PORT}:${SONARR_PORT}
+        restart: unless-stopped
+
+    radarr:
+        image: lscr.io/linuxserver/radarr:latest
+        container_name: radarr
+        environment:
+        - PUID=${PUID}
+        - PGID=${PGID}
+        - TZ=${TZ}
+        volumes:
+        - /etc/localtime:/etc/localtime:ro
+        - ${ROOT}/radarr/config:/config
+        ports:
+        - ${RADARR_PORT}:${RADARR_PORT}
+        restart: unless-stopped
+
+    overseerr:
+        image: lscr.io/linuxserver/overseerr:latest
+        container_name: overseerr
+        environment:
+        - PUID=${PUID}
+        - PGID=${PGID}
+        - LOG_LEVEL=debug
+        - TZ=${TZ}
+        - PORT=${OVERSERR_PORT}
+        volumes:
+        - ${ROOT}/overseerr/appdata/config:/config
+        ports:
+        - ${OVERSERR_PORT}:${OVERSERR_PORT}
+        restart: unless-stopped
+
+    flaresolverr:
+        image: ghcr.io/flaresolverr/flaresolverr:latest
+        container_name: flaresolverr
+        environment:
+        - LOG_LEVEL=${LOG_LEVEL:-info}
+        - LOG_HTML=${LOG_HTML:-false}
+        - CAPTCHA_SOLVER=${CAPTCHA_SOLVER:-none}
+        - TZ=${TZ}
+        ports:
+        - ${FLARESOLVERR_PORT}:${FLARESOLVERR_PORT}
+        restart: unless-stopped
 EOF
