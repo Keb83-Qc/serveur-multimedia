ad1f3ef3ab136dbd123438fcb598116b17433df6
Keb83-Qc <94471179+Keb83-Qc@users.noreply.github.com>
2023-11-08 13:25:25 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,16 +16,6 @@ DATA=/srv
 # plex https://www.plex.tv/claim/
 PLEXCLAIM=
     
-# Config Port
-QBT_WEBUI_PORT=8080
-TAUTULLI_PORT=8181
-JACKETT_PORT=8282
-SONARR_PORT=8383
-RADARR_PORT=8484
-OVERSERR_PORT=8585
-FLARESOLVERR_PORT=8686
-WIZARR_PORT=8787
-QBT_PORT=6881
 EOF
 
 echo "Fichier $_fichier créer avec succès."
@@ -34,66 +24,23 @@ _fichier="docker-compose.yml"
 cat <<- 'EOF' > $_fichier
 version: "3"
 services:
-tautulli:
-    image: lscr.io/linuxserver/tautulli:latest
-    container_name: tautulli
-    environment:
-    - PUID=${PUID}
-    - PGID=${PGID}
-    - TZ=${TZ}
-    volumes:
-    - ${ROOT}/tautulli:/config
-    ports:
-    - ${TAUTULLI_PORT}:${TAUTULLI_PORT}
-    restart: unless-stopped
-qbittorrent:
-    image: lscr.io/linuxserver/qbittorrent:latest
-    container_name: qbittorrent
-    environment:
-    - PUID=${PUID}
-    - PGID=${PGID}
-    - TZ=${TZ}
-    - QBT_WEBUI_PORT=${QBT_WEBUI_PORT}
-    volumes:
-    - ${ROOT}/qbittorent/config:/config
-    - ${DATA}/downloads:/downloads
-    ports:
-    - ${QBT_PORT}:${QBT_PORT}/tcp
-    - ${QBT_PORT}:${QBT_PORT}/udp
-    - ${QBT_WEBUI_PORT}:${QBT_WEBUI_PORT}
-    restart: unless-stopped
-jackett:
-    image: lscr.io/linuxserver/jackett:latest
-    container_name: jackett
-    environment:
-    - PUID=${PUID}
-    - PGID=${PGID}
-    - TZ=${TZ}
-    - AUTO_UPDATE=true
-    volumes:
-    - /etc/localtime:/etc/localtime:ro
-    - ${ROOT}/jackett/downloads:/downloads
-    - ${ROOT}/jackett/data:/config
-    ports:
-    - ${JACKETT_PORT}:${JACKETT_PORT}
-    restart: unless-stopped
-plex:
-    image: lscr.io/linuxserver/plex:latest
-    container_name: plex
-    network_mode: host
-    environment:
+  plex:
+  image: lscr.io/linuxserver/plex:latest
+  container_name: plex
+  network_mode: host
+  environment:
     - PUID=${PUID}
     - PGID=${PGID}
     - TZ=${TZ}
     - VERSION=docker
-    - PLEX_CLAIM=${PLEXCLAIM}   
-    volumes:
+    - PLEX_CLAIM=${PLEXCLAIM}
+  volumes:
     - ${ROOT}/plex/config:/config
     - ${ROOT}/plex/transcode:/transcode
     - ${ROOT}/plex/data:/data
     - ${DATA}/plex/tvseries:/tv
     - ${DATA}/plex/movies:/movies
-    ports:
+  ports:
     - 32400:32400
     - 1900:1900/udp
     - 5353:5353/udp
@@ -103,65 +50,104 @@ plex:
     - 32413:32413/udp
     - 32414:32414/udp
     - 32469:32469
-    restart: unless-stopped
-sonarr:
-    image: lscr.io/linuxserver/sonarr:latest
-    container_name: sonarr
+  restart: unless-stopped
+  tautulli:
+    image: lscr.io/linuxserver/tautulli:latest
+    container_name: tautulli
     environment:
-    - PUID=${PUID}
-    - PGID=${PGID}
-    - TZ=${TZ}
+      - PUID=${PUID}
+      - PGID=${PGID}
+      - TZ=${TZ}
     volumes:
-    - /etc/localtime:/etc/localtime:ro
-    - ${ROOT}/sonarr/config:/config
+      - ${ROOT}/tautulli:/config
     ports:
-    - ${SONARR_PORT}:${SONARR_PORT}
+      - 8181:8181
     restart: unless-stopped
-radarr:
-    image: lscr.io/linuxserver/radarr:latest
-    container_name: radarr
-    environment:
-    - PUID=${PUID}
-    - PGID=${PGID}
-    - TZ=${TZ}
-    volumes:
-    - /etc/localtime:/etc/localtime:ro
-    - ${ROOT}/radarr/config:/config
-    ports:
-    - ${RADARR_PORT}:${RADARR_PORT}
-    restart: unless-stopped
-overseerr:
+  overseerr:
     image: lscr.io/linuxserver/overseerr:latest
     container_name: overseerr
     environment:
-    - PUID=${PUID}
-    - PGID=${PGID}
-    - LOG_LEVEL=debug
-    - TZ=${TZ}
-    - PORT=${OVERSERR_PORT}
+      - PUID=${PUID}
+      - PGID=${PGID}
+      - TZ=${TZ}
     volumes:
-    - ${ROOT}/overseerr/appdata/config:/config
+      - ${ROOT}/overseerr/appdata/config:/config
     ports:
-    - ${OVERSERR_PORT}:${OVERSERR_PORT}
+      - 5055:5055
     restart: unless-stopped
-flaresolverr:
+  sonarr:
+    image: lscr.io/linuxserver/sonarr:latest
+    container_name: sonarr
+    environment:
+      - PUID=${PUID}
+      - PGID=${PGID}
+      - TZ=${TZ}
+    volumes:
+      - ${ROOT}/sonarr/config:/config
+    ports:
+      - 8989:8989
+    restart: unless-stopped
+  radarr:
+    image: lscr.io/linuxserver/radarr:latest
+    container_name: radarr
+    environment:
+      - PUID=${PUID}
+      - PGID=${PGID}
+      - TZ=${TZ}
+    volumes:
+      - ${ROOT}/radarr/config:/config
+    ports:
+      - 7878:7878
+    restart: unless-stopped
+  qbittorrent:
+    image: lscr.io/linuxserver/qbittorrent:latest
+    container_name: qbittorrent
+    environment:
+      - PUID=${PUID}
+      - PGID=${PGID}
+      - TZ=${TZ}
+      - WEBUI_PORT=8080
+    volumes:
+      - ${ROOT}/qbittorent/config:/config
+      - ${DATA}/downloads:/downloads
+    ports:
+      - 8080:8080
+      - 6881:6881
+      - 6881:6881/udp
+    restart: unless-stopped
+  jackett:
+    image: lscr.io/linuxserver/jackett:latest
+    container_name: jackett
+    environment:
+      - PUID=${PUID}
+      - PGID=${PGID}
+      - TZ=${TZ}
+      - AUTO_UPDATE=true
+    volumes:
+      - ${ROOT}/jackett/downloads:/downloads
+      - ${ROOT}/jackett/data:/config
+    ports:
+      - 9117:9117
+    restart: unless-stopped
+  flaresolverr:
+    # DockerHub mirror flaresolverr/flaresolverr:latest
     image: ghcr.io/flaresolverr/flaresolverr:latest
     container_name: flaresolverr
     environment:
-    - LOG_LEVEL=${LOG_LEVEL:-info}
-    - LOG_HTML=${LOG_HTML:-false}
-    - CAPTCHA_SOLVER=${CAPTCHA_SOLVER:-none}
-    - TZ=${TZ}
+      - LOG_LEVEL=${LOG_LEVEL:-info}
+      - LOG_HTML=${LOG_HTML:-false}
+      - CAPTCHA_SOLVER=${CAPTCHA_SOLVER:-none}
+      - TZ=${TZ}
     ports:
-    - ${FLARESOLVERR_PORT}:${FLARESOLVERR_PORT}
+      - "${PORT:-8191}:8191"
     restart: unless-stopped
-wizarr:
-  container_name: wizarr
-  image: ghcr.io/wizarrrr/wizarr:latest
-  ports:
-    - ${WIZARR_PORT}:${WIZARR_PORT}
-  volumes:
-    - ${ROOT}/wizarr/database:/data/database
+  wizarr:
+    container_name: wizarr
+    image: ghcr.io/wizarrrr/wizarr:latest
+    ports:
+      - 5690:5690
+    volumes:
+      - ${ROOT}/wizarr/database:/data/database
 EOF
 
 echo "Fichier $_fichier créer avec succès."
